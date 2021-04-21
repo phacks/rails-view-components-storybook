bbc4574857d435de301452ec2b9ad3fc224dcd81
Nicolas Goutay <nicolas.goutay@gmail.com>
2021-04-21 17:37:05 +0200

    Update preview to accept `type`

--- test/components/previews/button_component_preview.rb
+++ test/components/previews/button_component_preview.rb
@@ -1,5 +1,19 @@
 class ButtonComponentPreview < ViewComponent::Preview
-  def default
-    render(ButtonComponent.new)
+  def default(type: :primary)
+    type = type.to_sym if type
+
+    render(ButtonComponent.new(type: type)) { 'Button' }
+  end
+
+  def primary
+    render(ButtonComponent.new(type: :primary)) { 'Submit' }
+  end
+
+  def outline
+    render(ButtonComponent.new(type: :outline)) { 'Cancel' }
+  end
+
+  def danger
+    render(ButtonComponent.new(type: :danger)) { 'Delete' }
   end
 end
